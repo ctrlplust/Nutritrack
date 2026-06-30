56e049538ef2c24b48b6592a050607081dec12f1
Tomás Núñez <tomi@nutritrack.dev>
2026-06-29 21:16:36 -0400

    Actualizar URL Pinggy: nqiht

--- esp32/config.example.h
+++ esp32/config.example.h
@@ -6,6 +6,6 @@
 
 #define WIFI_SSID        "Pochita"
 #define WIFI_PASSWORD    "4c3opij0km"
-#define SERVER_BASE      "https://bqxkh-201-188-79-192.run.pinggy-free.link/api/nutritrack"
+#define SERVER_BASE      "https://nqiht-201-188-79-192.run.pinggy-free.link/api/nutritrack"
 
 #endif
